9f7a98cd38c94ec22afd4d35dbaece0c95c61ad8
HJliu1123 <2062300584@qq.com@domain.com>
2016-05-03 17:28:08 +0800

    NSUserDefault

---
+++ April/NSUserDefault/NSUserDefault/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  NSUserDefault
+//
+//  Created by liuhj on 16/5/3.
+//  Copyright © 2016年 liuhj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var userName : NSString?
+    var password : NSString?
+    let userDefault = NSUserDefaults.standardUserDefaults()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setData()
+        
+        self.login()
+        
+        
+        
+    }
+    
+    func setData() {
+        
+        
+        userDefault.setObject("user", forKey: "userName")
+        userDefault.setObject("psd", forKey: "password")
+    }
+    
+    
+    func login() {
+        
+        userName = userDefault.valueForKey("userName") as? NSString
+        password = userDefault.valueForKey("password") as? NSString
+        
+        if userName == "user" && password == "psd" {
+            print("login success")
+        } else {
+            print("faile")
+        }
+        
+        
+    }
+    
+
+    
+    
+    
+    
+    
+
+}
+
