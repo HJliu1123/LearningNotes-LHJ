3266be7ee6d366a7575e6a0d0e366957dc33297d
HJliu1123 <2062300584@qq.com@domain.com>
2016-05-06 14:26:03 +0800

    FMDB 加注释

--- April/DataWithFMDB/DataWithFMDB/ViewController.swift
+++ April/DataWithFMDB/DataWithFMDB/ViewController.swift
@@ -95,6 +95,11 @@ class ViewController: UIViewController {
         return stus
     }
     //线程安全的
+    /*
+     FMDatabaseQueue虽然看似一个队列，实际上它本身并不是，它通过内部创建一个Serial的dispatch_queue_t来处理通过inDatabase和inTransaction传入的Blocks，所以当我们在主线程（或者后台）调用inDatabase或者inTransaction时，代码实际上是同步的。FMDatabaseQueue这么设计的目的是让我们避免发生并发访问数据库的问题，因为对数据库的访问可能是随机的（在任何时候）、不同线程间（不同的网络回调等）的请求。内置一个Serial队列后，FMDatabaseQueue就变成线程安全了，所有的数据库访问都是同步执行，而且这比使用@synchronized或NSLock要高效得多。
+     
+     但是这么一来就有了一个问题：如果后台在执行大量的更新，而主线程也需要访问数据库，虽然要访问的数据量很少，但是在后台执行完之前，还是会阻塞主线程。
+     */
     func safeAddData(stu : Student) {
         let queue : FMDatabaseQueue = FMDatabaseQueue(path: filePath)
         queue.inDatabase { (_db : FMDatabase!) in
